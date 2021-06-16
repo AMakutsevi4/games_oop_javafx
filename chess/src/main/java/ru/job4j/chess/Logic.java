63fca07e8555621b259fcc12d7ff9b9cd6ea3c83
AMakutsevi4 <Qwerty123>
2021-06-16 09:33:01 +0900

    2. Каркас шахматной доски [#792]

--- chess/src/main/java/ru/job4j/chess/Logic.java
+++ chess/src/main/java/ru/job4j/chess/Logic.java
@@ -2,6 +2,7 @@ package ru.job4j.chess;
 
 import ru.job4j.chess.firuges.Cell;
 import ru.job4j.chess.firuges.Figure;
+
 import java.util.Arrays;
 
 public final class Logic {
@@ -21,14 +22,15 @@ public final class Logic {
     }
 
     private boolean free(Cell[] steps) throws OccupiedCellException, FigureNotFoundException {
-        Figure[] figure = figures;
-        for (int i = 0; i < figure.length; i++) {
-            if (figure[i].position().equals(steps[i])) {
-                throw new OccupiedCellException();
+        for (Figure figure : figures) {
+            for (Cell cell : steps) {
+                if (figure.position().equals(cell)) {
+                    throw new OccupiedCellException();
+                }
             }
         }
-       return true;
-       }
+        throw new FigureNotFoundException();
+    }
 
     public void clean() {
         Arrays.fill(figures, null);
